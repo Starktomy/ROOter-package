a82f1f65d2a3605691abbd506fa726c563f67153
gaozhudian <37720599+Starktomy@users.noreply.github.com>
2023-03-18 15:58:16 +0800

    fix lua get error
    fix lua get error

--- rooter/ext-rooter-basic/files/usr/lib/lua/luci/controller/rebootmodem.lua
+++ rooter/ext-rooter-basic/files/usr/lib/lua/luci/controller/rebootmodem.lua
@@ -5,10 +5,10 @@ translate = I18N.translate
 
 function index()
         local page
-		local multilock = uci:get("custom", "multiuser", "multi") or "0"
-		local rootlock = uci:get("custom", "multiuser", "root") or "0"
+		local multilock = "0"
+		local rootlock =  "0"
 		if (multilock == "0") or (multilock == "1" and rootlock == "1") then
-			page = entry({"admin", "system", "rebootmodem"}, template("admin_system/rebootmodem"), _(translate("Reboot")), 93)
+			page = entry({"admin", "system", "rebootmodem"}, template("admin_system/rebootmodem"), _(translate("Reboot Modem")), 93)
 			page.dependent = true
 		else
 			page = entry({"admin", "system", "rebootmodem"}, template("admin_system/rebootmodem"), _(translate("Restart Router")), 93)
